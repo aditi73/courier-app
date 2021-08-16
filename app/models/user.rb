1c8a1547a4b09eb12246d0feb07a0b309140ba85
Aditi Thakur <a.thakur73.at@gmail.com>
2021-08-17 01:45:27 +0530

    added additional fields for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :user_detail
+
+  accepts_nested_attributes_for :user_detail, :reject_if => :all_blank
 end
